617c8c9be67bad3e9bddcbcb9fd11b3a0ea6b7ac
Jeevan <jeevan2961@gmail.com>
2022-08-06 01:32:57 +0530

    world app setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,6 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'pages/home.dart';
 
 void main() {
-  runApp(const MaterialApp (home: QuoteList()));
+  runApp(const MaterialApp (home: Home()));
 }
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Quote> quotes = [
-    Quote(author:'jeevan',text:"The purpose of our lives is to be happy."),
-    Quote(author:'naveen',text:"Life is what happens when you're busy making other plans."),
-    Quote(author:'satish',text:"Get busy living or get busy dying."),    
-  ];
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("Awesome Quotes"),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((quote) => QuoteCard(
-          quote:quote,
-          delete:(){
-            setState(() {
-              quotes.remove(quote);
-            });
-          }
-          )).toList()
-      ),
-    );
-  }
-}
-
