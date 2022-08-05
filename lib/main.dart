3dabb7953c3a3a790e96916072e685f92d4a4c6f
Jeevan <jeevan2961@gmail.com>
2022-08-05 21:27:44 +0530

    ninja id card

--- lib/main.dart
+++ lib/main.dart
@@ -2,60 +2,99 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp (home: Home()));
+  runApp(const MaterialApp (home: NinjaCard()));
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('InTrade'),
-          centerTitle: true,
-          backgroundColor: Colors.red,
-        ),
-        body: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Image.asset('assets/photo-1614226114656-998786bf0955.avif')
-              ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.cyan,
-                child: Text("1"),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.pinkAccent,
-                child: Text("2"),
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: const Text("Ninja ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images.jpeg'),
+                radius: 40,
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.amber,
-                child: Text("3"),
+            Divider(
+              height: 80,
+              color: Colors.green,
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2
               ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "CHONG-LE",
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2
+              ),
+            ),
+            SizedBox(height: 30),
+                       Text(
+              "NINJA LEVEL",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "8",
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10),
+                Text(
+                  "Chongle@gmail.com",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                    letterSpacing: 1
+                  ),
+                  )
+              ],
             )
-          ],
+          ]
           ),
-          // Cant apply margin or color for only padding widget
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.red,
-          child: const Text("Press"),),
-      );
+      ),
+    );
   }
 }
