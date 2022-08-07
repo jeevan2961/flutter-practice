740e915f45f56cab155e4293a74367af8a034225
Jeevan <jeevan2961@gmail.com>
2022-08-07 15:10:04 +0530

    cocoapods issue resolved with macos

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../services/world_time.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
+import 'package:geolocator/geolocator.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,18 +11,20 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
-  void setupworldTime() async{
-    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+  void setupworldTime() async {
+    // Position position = await Geolocator.getCurrentPosition(
+    //     desiredAccuracy: LocationAccuracy.high);
+    // print(position);
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    Navigator.pushReplacementNamed(context, '/home',arguments: {
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location': instance.location,
-      'flag':instance.flag,
-      'time':instance.time,
-      'isdaytime':instance.isDayTime
-      });
-    }
-  
+      'flag': instance.flag,
+      'time': instance.time,
+      'isdaytime': instance.isDayTime
+    });
+  }
 
   @override
   void initState() {
@@ -30,18 +32,16 @@ class _LoadingState extends State<Loading> {
     setupworldTime();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[700],
-      body: Center(
-        child: SpinKitRotatingCircle(
+        backgroundColor: Colors.blue[700],
+        body: Center(
+            child: SpinKitRotatingCircle(
           size: 80,
           color: Colors.white,
         )
-        //CircularProgressIndicator(),
-        )
-    );
+            //CircularProgressIndicator(),
+            ));
   }
 }
